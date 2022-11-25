781c4d3ae7cf4e8908f528dd5f782db3cf88e914
Varshita Nukala <56291769+VarshitaNukala@users.noreply.github.com>
2022-11-26 00:45:14 +0530

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,31 +1,26 @@
-import BookShelfChanger from "./BookShelfChanger";
-const Book = ({ bookData, changeBookData,returnedFromSearch }) => {
-  return (
-    <li>
-      <div className="book">
-        {bookData.imageLinks && (
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 188,
-                backgroundImage: `url(${bookData.imageLinks.thumbnail})`,
-              }}
-            ></div>
+import PropTypes from "prop-types";
+const BookShelfChanger = ({ bookData, changeBookData, returnedFromSearch }) => {
+  const handleChange = (event) => {
+    changeBookData(bookData, event.target.value);
+  };
 
-            <BookShelfChanger
-              bookData={bookData}
-              changeBookData={changeBookData}
-              returnedFromSearch={returnedFromSearch}
-            ></BookShelfChanger>
-          </div>
-        )}
-        <div className="book-title">{bookData.title}</div>
-        <div className="book-authors">{bookData.authors}</div>
-      </div>
-    </li>
+  return (
+    <div className="book-shelf-changer">
+      <select defaultValue={bookData.shelf} onChange={handleChange}>
+        <option disabled>Move to...</option>
+        <option value="none">None</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+      </select>
+    </div>
   );
 };
 
-export default Book;
+export default BookShelfChanger;
+
+BookShelfChanger.propTypes = {
+  bookData: PropTypes.object,
+  changeBookData: PropTypes.func,
+  returnedFromSearch: PropTypes.bool,
+};
